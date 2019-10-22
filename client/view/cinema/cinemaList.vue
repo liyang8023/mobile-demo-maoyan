205c51d580dfee419d2d4e374928a1dfab6289ee
liyang_bj1 <liyang_bj1@ysstech.com>
2019-10-22 18:09:36 +0800

    'cinema'

---
+++ client/view/cinema/cinemaList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="cinemaList">
+    <div class="block">
+      <h3 class="left">{{cinema.name}}</h3>
+      <p class="right">
+        <span>{{cinema.sPrice}}</span>起
+      </p>
+    </div>
+    <div class="block">
+      <p class="left">{{cinema.address}}</p>
+      <p class="right">{{cinema.distance}}</p>
+    </div>
+    <div class="feature">
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    cinema: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+<style lang="scss">
+@import '@/assets/scss/main.scss';
+  .cinemaList {
+    .block {
+      display: flex;
+      margin-top: 6px;
+      .left {
+        flex: 1;
+      }
+      h3 {
+        margin: 0;
+        font-size: 16px;
+      }
+      .right {
+        color: $explain-font;
+        span {
+          color: $main-red;
+          font-size: 16px;
+          padding-right: 5px;
+        }
+      }
+    }
+    
+  }
+</style>
--- client/view/index.vue
+++ client/view/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="indexPage">
-    <PageTitle :backShow="false" v-show="content !== 2">猫眼电影</PageTitle>
+    <PageTitle :backShow="false" v-show="content !== 2">{{titleText}}</PageTitle>
     <div class="centerContent">
       <MovieIndex v-show="content === 0"></MovieIndex>
-      <div v-show="content === 1">影院</div>
+      <CinemaIndex v-show="content === 1">影院</CinemaIndex>
       <MineIndex v-show="content === 2"></MineIndex>
     </div>
     <TabBar @changeContent="changeContent"></TabBar>
@@ -14,23 +14,35 @@ import TabBar from "@/components/tabBar";
 import PageTitle from "@/components/pageTitie";
 import MovieIndex from "@/view/movie/movieIndex";
 import MineIndex from '@/view/user/mineIndex';
+import CinemaIndex from '@/view/cinema/cinemaIndex';
 export default {
   name: "index",
   components: {
     TabBar,
     MovieIndex,
     PageTitle,
-    MineIndex
+    MineIndex,
+    CinemaIndex
   },
   data() {
     return {
-      content: 0
+      content: 0,
+      titleText: '猫眼电影'
     };
   },
   methods: {
     // 底部tab组件切换、内容变化
     changeContent(content) {
       this.content = content;
+    }
+  },
+  watch: {
+    content() {
+      if (this.content === 0) {
+        this.titleText = '猫眼电影';
+      } else if (this.content === 1) {
+        this.titleText = '影院';
+      }
     }
   }
 };
